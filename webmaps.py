331812d26740472018b94328be485fe9fe72cdb6
Lottosak <40791314+Lottosak@users.noreply.github.com>
2018-10-05 14:23:16 +0200

    Adding popilation layer to folium map

--- webmaps.py
+++ webmaps.py
@@ -3,16 +3,35 @@ import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
 
-lat = list(data["LAT"])
-lon = list(data["LON"])
+def marker_color(elevation):
+    if elevation < 1000:
+        return "green"
+    elif elevation < 3000:
+        return "orange"
+    else:
+        return "red"
 
 map = folium.Map(location=[41.2242,-115.597304], zoom_start=6, tiles="Mapbox bright")
 
-fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt,ln], popup="Marker", icon=folium.Icon(color="green")))
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json','r', encoding='utf-8-sig').read(),
+    style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
 
-map.add_child(fg)
+fgm = folium.FeatureGroup(name="Markers")
+for place in data.iterrows():
+    fgm.add_child(
+        folium.CircleMarker(location=[place[1]['LAT'],place[1]['LON']],
+        radius=6,
+        popup=str(place[1]['ELEV'])+"m",
+        fill_color=marker_color(place[1]['ELEV']),
+        color="grey",
+        fill_opacity=0.7
+        )
+    )
 
+map.add_child(fgp)
+map.add_child(fgm)
+map.add_child(folium.LayerControl())
 map.save("webmap.html")
