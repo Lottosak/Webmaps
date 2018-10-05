ef8555ea219218cdfcfc4f054f987d4cdf85ac26
Lottosak <40791314+Lottosak@users.noreply.github.com>
2018-10-05 12:18:00 +0200

    added markers to map

--- webmaps.py
+++ webmaps.py
@@ -1,2 +1,18 @@
-msg = "hello world"
-print(msg)
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+map = folium.Map(location=[41.2242,-115.597304], zoom_start=6, tiles="Mapbox bright")
+
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Marker", icon=folium.Icon(color="green")))
+
+map.add_child(fg)
+
+map.save("webmap.html")
